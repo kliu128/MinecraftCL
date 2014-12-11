7b93448f57406529b4d68fe34774fa5a0cfda57f
Kevin Liu <batchfiles99@gmail.com>
2014-12-11 17:14:15 -0500

    test

--- MinecraftCLBootstrap/Program.cs
+++ MinecraftCLBootstrap/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,6 +14,7 @@ namespace MinecraftCLBootstrap
     {
         static void Main(string[] args)
         {
+            // Print information about environment and bootstrap version.
             Console.Title = "Minecraft CL Bootstrap";
             Console.WriteLine("MinecraftCL Bootstrap v" + Application.ProductVersion);
             Console.WriteLine("Current time is " + DateTime.UtcNow + " UTC");
@@ -21,31 +23,70 @@ namespace MinecraftCLBootstrap
             Console.WriteLine();
             Console.WriteLine(".NET Version       = " + System.Environment.Version);
 
+            // Load MinecraftCL version information from the file.
+            string minecraftCLVersion = null;
             if (File.Exists("CL.exe"))
-                Console.WriteLine("MinecraftCL Status = Installed");
+            {
+                // Get the MinecraftCL version if it is already downloaded.
+                minecraftCLVersion = FileVersionInfo.GetVersionInfo("CL.exe").FileVersion;
+                Console.WriteLine("MinecraftCL Version = " + minecraftCLVersion);
+            }
             else
             {
-                // MinecraftCL is not downloaded, begin download
-                Console.WriteLine("MinecraftCL Status = Not Installed");
-                Console.WriteLine("Downloading MinecraftCL...");
+                // MinecraftCL is not downloaded.
+                Console.WriteLine("MinecraftCL Version = Not Installed");
             }
+            // Check for updates to MinecraftCL.
+            bool updateSuccess = checkForUpdate(minecraftCLVersion);
+            if (updateSuccess == true || File.Exists("CL.exe"))
+                Process.Start("CL.exe");
+        }
 
+        /// <summary>
+        /// Checks for MinecraftCL update.
+        /// </summary>
+        /// <param name="minecraftCLVersion"></param>
+        /// <returns>Returns false if update failed. Does NOT return false if there was no update required.</returns>
+        private static bool checkForUpdate(string minecraftCLVersion)
+        {
+            Version latestVersion = null;
             using (WebClient client = new WebClient())
             {
-                Version latestVersion;
                 try
                 {
+                    // Download the Latest Version file from the server, to get the latest MinecraftCL version.
                     latestVersion = Version.Parse(client.DownloadString(ConfigurationManager.AppSettings["LatestVersionURL"]));
                     Console.WriteLine("MinecraftCL Latest Version = " + latestVersion);
                 }
                 catch
                 {
+                    // Could not reach server; is it down?
                     Console.WriteLine("Could not check for update.");
+                    return false;
                 }
+
+                // If MinecraftCL is not downloaded (null), or the latest version is higher than the installed version,
+                // download MinecraftCL from server.
+                if ((minecraftCLVersion == null) || 
+                    (minecraftCLVersion != null && latestVersion > Version.Parse(minecraftCLVersion)))
+                {
+                    try
+                    {
+                        // Download MinecraftCL
+                        client.DownloadFile(ConfigurationManager.AppSettings["CLDownloadURL"], "CL.exe");
+                        Console.WriteLine("MinecraftCL successfully updated to " + latestVersion + ".");
+                        return true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error downloading MinecraftCL version.");
+                        return false;
+                    }
+                }
+                else
+                    // No update needed.
+                    return true;
             }
-            
-            
-            Console.ReadLine();
         }
     }
 }
