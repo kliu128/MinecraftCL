2674bf0c924ea45ee40786aebc93852f55841242
Kevin Liu <batchfiles99@gmail.com>
2015-02-08 15:12:40 -0500

    Added NativeMethods cs file.

---
+++ MinecraftCL/NativeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MinecraftCL
+{
+    public static class NativeMethods
+    {
+        public const int GWL_STYLE = -16;
+        public const int WS_SYSMENU = 0x80000;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    }
+}
