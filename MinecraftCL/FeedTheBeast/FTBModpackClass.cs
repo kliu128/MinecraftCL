95862d8b2e7e440d5490a37e4e5f113fcf1d0f41
Kevin Liu <batchfiles99@gmail.com>
2015-03-01 21:11:41 -0500

    Removed /// <remarks /> that were auto-generated by Paste as Xml.

--- MinecraftCL/FeedTheBeast/FTBModpackClass.cs
+++ MinecraftCL/FeedTheBeast/FTBModpackClass.cs
@@ -10,71 +10,54 @@ namespace MinecraftCL.FeedTheBeast
     [XmlRoot("modpacks")]
     public partial class FTBModpackList
     {
-        /// <remarks/>
         public object script { get; set; }
 
-        /// <remarks/>
         [XmlElementAttribute("modpack")]
         public FTBModpack[] modpack { get; set; }
     }
 
-    /// <remarks/>
     [XmlTypeAttribute()]
     public class FTBModpack : Modpack
     {
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public override string author { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string description { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string image { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string logo { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string mods { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public override string name { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string oldVersions { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string repoVersion { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string serverPack { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string squareImage { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string url { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string version { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public string dir { get; set; }
 
-        /// <remarks/>
         [XmlAttributeAttribute()]
         public override string mcVersion { get; set; }
     }
